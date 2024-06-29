1f55919b7369709c60e80e6487104cbb2295ef7b
Daniel Eichman <d.eichman@outlook.com>
2024-06-28 23:23:57 -0700

    Send wife messages with gpt

--- cdk/stacks/lambdaStack.ts
+++ cdk/stacks/lambdaStack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import { Architecture, Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Rule, RuleTargetInput, Schedule } from 'aws-cdk-lib/aws-events';
 import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
+import { Duration } from 'aws-cdk-lib';
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,15 +14,19 @@ export class LambdaStack extends cdk.Stack {
       handler: 'bootstrap',
       runtime: Runtime.PROVIDED_AL2,
       architecture: Architecture.ARM_64,
+      timeout: Duration.minutes(3),
       environment: {
         FINNHUB_API_KEY: process.env.FINNHUB_API_KEY!,
         PUSHOVER_APP_KEY: process.env.PUSHOVER_APP_KEY!,
         PUSHOVER_RECIPIENT: process.env.PUSHOVER_RECIPIENT!,
+        WIFE_RECIPIENT: process.env.WIFE_RECIPIENT!,
+        CHAT_GPT_API_KEY: process.env.CHAT_GPT_API_KEY!,
       },
     });
 
     const eventRule = new Rule(this, 'scheduleRule', {
-      schedule: Schedule.expression('cron(5 14-20 ? * MON-FRI *)'),
+      // schedule: Schedule.expression('cron(5 14-20 ? * MON-FRI *)'),
+      schedule: Schedule.expression('cron(5 14-20 ? * * *)'),
     });
     eventRule.addTarget(new LambdaFunction(lambda, { event: RuleTargetInput.fromObject({}) }));
   }
--- lambda/pkg/main.go
+++ lambda/pkg/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gregdel/pushover"
 	"lambda/pkg/apis"
 	"lambda/pkg/service"
 	"os"
 )
 
 const PUSHOVER_RECIPIENT = "PUSHOVER_RECIPIENT"
+const WIFE_RECIPIENT = "WIFE_RECIPIENT"
 
 func main() {
 	lambda.Start(router)
@@ -20,9 +22,29 @@ func router(ctx context.Context, req map[string]string) (string, error) {
 	tickerService := service.NewTickerService(stockApi, notificationApi)
 
 	output, err := tickerService.Run()
-
 	if err != nil {
 		return err.Error(), err
 	}
-	return output, nil
+
+	gptApi := apis.NewChatGptApi()
+	gptRes, err := gptApi.GenerateMessageToWife()
+	if err != nil {
+		return err.Error(), err
+	}
+	messageRequest := pushover.Message{
+		Message:  gptRes,
+		Title:    "I lovee you",
+		Priority: pushover.PriorityNormal,
+	}
+	wifeNotificationApi := apis.NewNotificationApi(os.Getenv(WIFE_RECIPIENT))
+	_, err = wifeNotificationApi.Send(&messageRequest)
+	if err != nil {
+		return err.Error(), err
+	}
+	_, err = notificationApi.Send(&messageRequest)
+	if err != nil {
+		return err.Error(), err
+	}
+
+	return output + gptRes, nil
 }
